aa9e902a15f185ceb6d46616a12f9995b0b8f789
Lhowelle <rmf24sju@uea.ac.uk>
2024-11-07 13:50:02 +0000

    trying json

---
+++ JS/teams.js
@@ -0,0 +1,28 @@
+document.querySelector(".member h4").textContent = "Testing"
+
+let localJsonFile = "JSON/teamdata.json";
+
+let sectionElement = document.querySelector(".member")
+
+document.addEventListener('DOMContentLoaded', ()=>{
+	fetch(localJsonFile)
+	.then(response => response.json())
+	.then (responseData =>{ 
+		
+		for (item of responseData){
+		const schedule = document.createElement('article');
+		console.log(schedule)
+		sectionElement.appendChild(schedule)
+		const test = document.createElement('h1');
+		test.textContent = item.name;
+		schedule.appendChild(test);
+
+		
+	
+	
+	}})
+	
+	
+	
+	
+	})
