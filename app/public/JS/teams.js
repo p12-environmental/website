7447fa70951af78d13fc6a84fba823dbeb172b5f
Lhowelle <rmf24sju@uea.ac.uk>
2024-11-18 20:34:34 +0000

    fixing team page image and roles

--- app/public/JS/teams.js
+++ app/public/JS/teams.js
@@ -11,22 +11,22 @@ class MemberItem extends HTMLElement {
 		this.img.alt = "profile image";
 
 		this.figcaption = document.createElement("figcaption");
-		this.nameHeading = document.createElement("h4");
+		this.nameHeading = document.createElement("h3");
+		this.roleItem = document.createElement("h4");
 
 		this.detailsList = document.createElement("ul");
-		this.roleItem = document.createElement("li")
 		this.biographyItem = document.createElement("li")
 		this.responsibilityItem = document.createElement("li")
 		this.contributionsItem = document.createElement("li")
 
 		this.detailsList.append(this.biographyItem, this.responsibilityItem, this.contributionsItem);
-		this.figcaption.append(this.nameHeading, this.detailsList);
+		this.figcaption.append(this.nameHeading,this.roleItem, this.detailsList);
 		this.figure.append(this.img, this.figcaption);
 		this.append(this.figure);
 	}
 
 	connectedCallback() {
-		this.img.src = "/assets/default-profile.png";
+		this.img.src = this.getAttribute("image");
 		this.nameHeading.textContent = this.getAttribute("name");
 		this.roleItem.textContent = this.getAttribute("role");
 		this.biographyItem.textContent = this.getAttribute("biography");
@@ -44,6 +44,7 @@ document.addEventListener("DOMContentLoaded", () => {
 		.then(responseData => {
 			for (item of responseData) {
 				const memberItem = document.createElement("article", { is: "member-item" });
+				memberItem.setAttribute("image", item.image);
 				memberItem.setAttribute("name", item.name);
 				memberItem.setAttribute("role", item.role);
 				memberItem.setAttribute("biography", item.biography);
